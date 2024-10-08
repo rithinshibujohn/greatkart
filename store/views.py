1dce8890d247a7dbce8607a308581ce878cb11fb
rithinshibujohn <125265236+rithinshibujohn@users.noreply.github.com>
2024-10-08 16:57:49 +0530

    PRODUCT GALLERY WITH UNLIMITED IMAGES COMPLETED

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404, redirect
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating, ProductGallery
 from category.models import Category
 from carts.models import CartItem, Cart
 from carts.views import _cart_id
@@ -29,9 +29,14 @@ def store(request, category_slug=None):
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
+
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)    
+    
     context = {
         'products': paged_products,
-         'product_count': product_count
+         'product_count': product_count,
+         'reviews': reviews
     }
     return render(request,'store/store.html',context)
 
@@ -51,11 +56,16 @@ def product_detail(request, category_slug, product_slug):
 
     #get the review
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)    
+
+    #get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    print(product_gallery)
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery
     }
     return render(request, 'store/product_detail.html',context)
 
