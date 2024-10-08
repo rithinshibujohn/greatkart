100885b00d4eea3f230f2ce55f3fe8cbb5ab2ebf
rithinshibujohn <125265236+rithinshibujohn@users.noreply.github.com>
2024-10-08 17:49:02 +0530

    FINAL COMMIT

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
-    'orders'
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = ''
 
 ROOT_URLCONF = 'greatkart.urls'
 
