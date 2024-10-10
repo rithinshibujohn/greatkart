e2b0cf046e2b079344f7bf3ff6011e8b484b5e8c
rithinshibujohn <125265236+rithinshibujohn@users.noreply.github.com>
2024-10-10 11:39:35 +0530

    09-10-2024

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import HttpResponse,render
 from store.models import Product,ReviewRating
+from django.db.models import Prefetch
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('-created_date')
-
-    #get the review
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)    
-
+    # Fetch products with reviews in one query using prefetch_related
+    products = Product.objects.filter(is_available=True).order_by('-created_date').prefetch_related(
+        Prefetch('reviewrating_set', queryset=ReviewRating.objects.filter(status=True))
+    )
+    
     context = {
         'products': products,
-        'reviews': reviews
     }
-    return render(request,'home.html',context)
+    return render(request, 'home.html', context)
